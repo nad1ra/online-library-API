b016d79cfe9617f9e16d570cd94bf15b32d199a4
nad1ra <nodirakamaliddinovna@gamil.com>
2025-03-14 17:00:19 +0500

    done

--- books/serializers.py
+++ books/serializers.py
@@ -5,18 +5,18 @@ from .models import Book, BookCopy, BookLending
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ['id', 'title', 'authors', 'genre', 'isbn', 'published_date', 'desc', 'page_count', 'language']
 
 
 class BookCopySerializer(serializers.ModelSerializer):
     class Meta:
         model = BookCopy
-        fields = '__all__'
+        fields = ['id', 'book', 'inventory_number', 'condition', 'is_available', 'added_date']
 
 
 class BookLendingSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookLending
-        fields = '__all__'
+        fields = ['id', 'book_copy', 'borrower_name', 'borrower_email', 'borrowed_date', 'due_date', 'returned_date', 'status']
 
 
